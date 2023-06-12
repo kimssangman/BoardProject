19467f68fd606183cf8bda70a9120dc75a2fefdc
ksmnsmarts <ksm@nsmarts.co.kr>
2023-06-12 09:12:54 +0900

    [완료] 회원가입

---
+++ src/app/api/signUp/route.ts
@@ -0,0 +1,55 @@
+import { getNextSequenceValue } from '@/lib/db/counter/counter.model';
+import dbConnect from '@/lib/db/dbConnect';
+import User, { UserSchema } from '@/lib/db/user/user.model';
+import * as yup from 'yup';
+
+const bodySchema = yup.object().shape({
+    id: yup.string().required(),
+    name: yup.string().required(),
+    pw: yup.string().required(),
+});
+
+export async function POST(req: Request) {
+    const body = await req.json();
+    if (!bodySchema.isValidSync(body)) {
+        return new Response(JSON.stringify({ message: '회원가입에 실패함!' }), {
+            status: 400,
+        });
+    }
+
+    try {
+        /**--------------------------------------
+        * 데이터 유효성 검사
+        --------------------------------------*/
+        await bodySchema.validate(body);
+
+        /**--------------------------------------
+        * 회원가입 처리 로직
+        --------------------------------------*/
+        await dbConnect();
+        const { id, name, pw } = body;
+        const index = await getNextSequenceValue("index");
+
+        /**--------------------------------------
+        * password 해싱
+        --------------------------------------*/
+        const bcrypt = require('bcrypt');
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(pw, salt);
+
+        /**--------------------------------------
+        * 저장
+        --------------------------------------*/
+        const signUp = new User({ index, id, name, pw: hashedPass });
+        await signUp.save();
+
+        return new Response(JSON.stringify({ message: '회원가입에 성공함!' }), {
+            status: 200,
+        });
+    } catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify({ message: '회원가입에 실패함!' }), {
+            status: 500,
+        });
+    }
+}
---
+++ src/app/signUp/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignUpForm from '../components/SignUpForm'
+
+
+
+export default function page() {
+    return <section className='flex flex-col items-center'>
+        <SignUpForm></SignUpForm>
+    </section>
+
+}
